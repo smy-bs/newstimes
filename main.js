abd38f642b6ec9312f17b1e6ba52de5beec77626
Miyoung SB <bs.miyoung@gmail.com>
2024-02-21 11:43:31 +0100

    modification url

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY = `63f5f229f63e4668897bf96f197f7b39`;
 let news = [];
 const getLatesNews = async() => {
     const url = new URL (
-        `https://newstimes-noonaproject.netlify.app/v2/top-headlines?country=us&apikey=${API_KEY}`
+        `https://newstimes-noonaproject.netlify.app/top-headlines?category=business`
     );
 const response = await fetch(url);
 const data = await response.json();
