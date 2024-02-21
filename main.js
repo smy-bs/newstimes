dd6c051697fac1ac291db9377287368398d8c128
Miyoung SB <bs.miyoung@gmail.com>
2024-02-21 17:23:04 +0100

    api update

--- main.js
+++ main.js
@@ -1,13 +1,53 @@
+let navBar = document.querySelector(".nav-bars");
+let menuList = document.querySelector(".menus");
+navBar = addEventListener("click",() =>{
+menuList.classList.toggle('open');
+})
+menuList.addEventListener("click",() =>{
+    menuList.classList.remove('open');
+})
+
+const openSearchBox = () => {
+    let inputArea = document.getElementById("input-area");
+    if (inputArea.style.display === "inline") {
+      inputArea.style.display = "none";
+    } else {
+      inputArea.style.display = "inline";
+    }
+  };
+
 const API_KEY = `63f5f229f63e4668897bf96f197f7b39`;
-let news = [];
+let newsList = [];
 const getLatesNews = async() => {
     const url = new URL (
+        // `https://newsapi.org/v2/top-headlines?country=us&apikey=${API_KEY}`
         `https://newstimes-noonaproject.netlify.app/top-headlines?category=business`
     );
 const response = await fetch(url);
 const data = await response.json();
-news = data.articles;
-console.log("ddddd",news);
+newsList = data.articles;
+render();
+console.log("ddddd",newsList);
+};
+
+const render =() => {
+    const newsHTML = newsList.map(
+        (news) =>`<div class="row news">
+    <div class="col-lg-4">
+    <img class="news-img-size" 
+    src="${news.urlToImage}" alt="">
+    </div>
+        <div class="col-lg-8">
+        <h2> ${news.title}</h2>
+        <p>${news.description}</p>
+        <div>
+         ${news.source.name} * ${news.publishedAt}
+        </div>
+    </div> 
+</div>`
+).join("");
+    
+    document.getElementById('news-board').innerHTML = newsHTML;
 };
 
 getLatesNews () ; 
